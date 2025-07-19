b9feacc559502919dc4738ea73e4737457f25e4e
C043 <mariofragnitoph@gmail.com>
2025-07-19 18:03:16 +0200

    Changed the header

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,6 +3,6 @@
     class="flex justify-center items-center backdrop-blur-2xl shadow-sm border rounded-full border-white/20 p-10 bg-gray-500/20"
   >
     <img src="/raspLogo.png" class="h-40 me-5" />
-    <h1 class="archivo-black-bold">PiTV</h1>
+    <h1 class="archivo-black-bold">TV</h1>
   </div>
 </template>
