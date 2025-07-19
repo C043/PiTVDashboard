114b8e5dd93b34c675d81530bfad0ddb67ca3a37
C043 <mariofragnitoph@gmail.com>
2025-07-19 03:18:40 +0200

    Started working on the PiTV dashboard

---
+++ src/components/Tile.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps(["logo", "color", "href"]);
+</script>
+
+<template>
+  <a :href="href">
+    <div
+      class="flex justify-center items-center h-50 w-92 rounded-2xl backdrop-blur-sm border border-white/20 hover:scale-[1.1] transition-all shadow-sm"
+      :class="color"
+    >
+      <img :src="logo" class="h-40" />
+    </div>
+  </a>
+</template>
---
+++ src/components/TileContainer.vue
@@ -0,0 +1,33 @@
+<script setup>
+import Tile from "./Tile.vue";
+</script>
+
+<template>
+  <div class="bg-red-100 h-dvh">
+    <div class="flex flex-wrap justify-center gap-5 container m-auto pt-10">
+      <Tile
+        logo="../../public/netflixLogo.png"
+        color="bg-[#8E0000]/30"
+        href="https://www.netflix.com"
+      />
+
+      <Tile
+        logo="../../public/netflixLogo.png"
+        color="bg-[#8E0000]/30"
+        href="https://www.netflix.com"
+      />
+
+      <Tile
+        logo="../../public/netflixLogo.png"
+        color="bg-[#8E0000]/30"
+        href="https://www.netflix.com"
+      />
+
+      <Tile
+        logo="../../public/netflixLogo.png"
+        color="bg-[#8E0000]/30"
+        href="https://www.netflix.com"
+      />
+    </div>
+  </div>
+</template>
